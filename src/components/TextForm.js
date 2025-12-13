755101ef3c487c442860dae3f4dbc63fc63035f3
Sayali Rajwade <sayalirajwade077@gmail.com>
2025-12-13 17:22:47 +0530

    added speak, capitalize, cleartext button

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -18,6 +18,27 @@ export default function TextForm(props) {
     setText(event.target.value);
   }
 
+  const speak = () => {
+  window.speechSynthesis.cancel(); // stop previous speech
+  let msg = new SpeechSynthesisUtterance(text);
+  window.speechSynthesis.speak(msg);
+  };
+
+  const capitalFirstLetter = ()=>{
+        let words = text.split(" ")
+       let uppercaseword = ' '
+        words.forEach(element => {
+           uppercaseword += element.charAt(0).toUpperCase() + element.slice(1) + " "
+        });
+        setText(uppercaseword)
+    };
+    
+  const handleClearText =() =>{
+    // console.log("Uppercase was clicked " + text);
+    let newText = " ";
+    setText (newText);
+  }
+  
   const [text, setText] = useState(''); //state
   // text = "new text" // Wrong way to change the state
   // setText ("new text"); // Correct way to change the state
@@ -31,6 +52,10 @@ export default function TextForm(props) {
     </div>
     <button className="btn btn-primary mx-1" onClick={handleUpClick}>Covert to Uppercase</button>
     <button className="btn btn-primary mx-1" onClick={handleLoClick}>Covert to Lowercase</button>
+    <button className="btn btn-primary mx-2 my-2" type="submit" onClick={speak} >Speak</button>
+    <button className="btn btn-primary mx-2 my-2" type="submit" onClick={capitalFirstLetter} >Capitalize</button>
+    <button className="btn btn-primary mx-1" onClick={handleClearText}>Clear Text</button>
+
 
 </div>
 <div className="container my-3">
