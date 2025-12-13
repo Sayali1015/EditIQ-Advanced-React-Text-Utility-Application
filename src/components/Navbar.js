5c5a5ff37460da59a742d75049a0eed97f4f9706
Sayali Rajwade <sayalirajwade077@gmail.com>
2025-12-13 16:23:13 +0530

    added counters like words, characters,preview, added convert lowercase button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types'
 
 export default function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    // <nav className="navbar navbar-expand-lg bg-body-tertiary"> //=========Light NavBar
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">  
+
   <div className="container-fluid">
     <a className="navbar-brand" href="/">{props.title}</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
@@ -22,7 +24,7 @@ export default function Navbar(props) {
       </ul>
       <form className="d-flex" role="search">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
+        <button className="btn btn-outline-primary" type="submit">Search</button>
       </form>
     </div>
   </div>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,27 +7,41 @@ export default function TextForm(props) {
     setText (newText);
   }
 
+    const handleLoClick =() =>{
+    // console.log("Uppercase was clicked " + text);
+    let newText = text.toLowerCase();
+    setText (newText);
+  }
+
   const handleOnChange =(event) =>{
     // console.log("On Change");
     setText(event.target.value);
   }
 
-  const [text, setText] = useState('Enter text here'); //state
+  const [text, setText] = useState(''); //state
   // text = "new text" // Wrong way to change the state
   // setText ("new text"); // Correct way to change the state
 
-
   return (
-    <div>
+    <>
+    <div className="container">
         <h1>{props.heading} </h1>
         <div className="mb-3">
         <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
     </div>
-    <button className="btn btn-primary" onClick={handleUpClick}>
-      Covert to Uppercase
-      </button>
+    <button className="btn btn-primary mx-1" onClick={handleUpClick}>Covert to Uppercase</button>
+    <button className="btn btn-primary mx-1" onClick={handleLoClick}>Covert to Lowercase</button>
+
 </div>
-  )
+<div className="container my-3">
+    <h2>Your Text Summary</h2>
+    <p>{text.split(" ").length} words and {text.length} characters</p>
+    <p>{0.008 * text.split(" ").length } Minutes read</p>
+    <h2>Preview</h2>
+    <p>{text}</p>
+</div>
+</>
+ )
 }
 
 
