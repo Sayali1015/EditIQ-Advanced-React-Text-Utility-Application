bf09fb998807c2eeb8b699143667174b2bfca1ff
Sayali Rajwade <sayalirajwade077@gmail.com>
2025-12-14 20:52:32 +0530

    added Remove Extra Space and Copy text feature too the app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
+
 import Navbar from "./components/Navbar";
-// import TextForm from './components/TextForm'
+import TextForm from './components/TextForm'
 
 function App() {
   return (
@@ -9,12 +10,11 @@ function App() {
       <Navbar title = "TypoFix" aboutText = "About TypoFix"/>
 
       <div className="container my-3">
-        {/* <TextForm heading ="Enter the text to analyze below"/> */}
-        <About/>
+        <TextForm heading ="Enter the text to analyze below"/>
+        {/* <About/> */}
       </div>
       
     </>
-   
   );
 }
 
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -32,6 +32,17 @@ export default function TextForm(props) {
         });
         setText(uppercaseword)
     };
+
+    const handleCopy = ()=>{
+      var text = document.getElementById("myBox");
+      text.select();
+      navigator.clipboard.writeText(text.value);
+    };
+
+    const handleExtraSpace = () =>{
+      let newText = text.split(/[ ]+/);
+      setText(newText.join(" "))
+    };
     
   const handleClearText =() =>{
     // console.log("Uppercase was clicked " + text);
@@ -55,6 +66,9 @@ export default function TextForm(props) {
     <button className="btn btn-primary mx-2 my-2" type="submit" onClick={speak} >Speak</button>
     <button className="btn btn-primary mx-2 my-2" type="submit" onClick={capitalFirstLetter} >Capitalize</button>
     <button className="btn btn-primary mx-1" onClick={handleClearText}>Clear Text</button>
+    <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
+    <button className="btn btn-primary mx-1" onClick={handleExtraSpace}>Remove Extra Space</button>
+
 
 
 </div>
